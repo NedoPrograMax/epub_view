fb3fe9e245d4c1b523b017fc20b9c0f6d0dfb3a6
Maksym Loia <knemo7362@gmail.com>
2023-09-29 19:29:31 +0300

    test

--- lib/epub_view.dart
+++ lib/epub_view.dart
@@ -4,3 +4,6 @@ export 'src/ui/actual_chapter.dart';
 export 'src/ui/epub_view.dart';
 export 'src/ui/table_of_contents.dart';
 export 'src/helpers/epub_document.dart';
+export 'src/data/models/last_place_model.dart';
+export 'src/data/models/reader_result.dart';
+
