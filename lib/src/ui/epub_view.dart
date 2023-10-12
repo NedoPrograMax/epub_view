031b1618403b944db95b6cd49ea03fbca6f0df87
Maksym Loia <knemo7362@gmail.com>
2023-10-12 18:34:46 +0300

    t

--- lib/src/ui/epub_view.dart
+++ lib/src/ui/epub_view.dart
@@ -219,8 +219,9 @@ class _EpubViewState extends State<EpubView> {
     final aligment = currentPercent *
         ((firstItem?.itemLeadingEdge ?? 0).abs() +
             (firstItem?.itemTrailingEdge ?? 0).abs());
+    // +10k is needed so the percent is > 0. then it -
     final countedLastPlace = LastPlaceModel(
-      percent: position.itemLeadingEdge + 100,
+      percent: position.itemLeadingEdge + 10000,
       index: position.index + 1,
     );
     /*  final lastPlace = repository.lastReadResult.lastPlace == null ||
@@ -470,7 +471,8 @@ class _EpubViewState extends State<EpubView> {
     return ScrollablePositionedList.builder(
       shrinkWrap: widget.shrinkWrap,
       initialScrollIndex: (_controller.lastResult.lastPlace?.index ?? 1) - 1,
-      initialAlignment: (_controller.lastResult.lastPlace?.percent ?? 0) - 100,
+      initialAlignment:
+          (_controller.lastResult.lastPlace?.percent ?? 0) - 10000,
       itemCount: _paragraphs.length,
       itemScrollController: _itemScrollController,
       itemPositionsListener: _itemPositionListener,
