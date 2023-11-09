95ca7acb21ed7a9d251327c96bc0a675d9754a23
Maksym Loia <knemo7362@gmail.com>
2023-11-09 13:59:15 +0200

    t

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Programs\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\1234\FlutterProjets\work\kuka-real\epub_view"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=3.2.0"
+export "FLUTTER_BUILD_NUMBER=3.2.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
